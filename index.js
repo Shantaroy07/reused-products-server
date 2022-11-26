dd735ced071a0dcb52925acc0869fc731f351f22
Shantaroy07 <shantaroy148@gmail.com>
2022-11-26 14:28:21 +0600

    get category

--- index.js
+++ index.js
@@ -9,10 +9,40 @@ app.use(cors());
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.onsb2w9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const categoryCollection = client.db('reused-products').collection('categories')
+        const productCollection = client.db('reused-products').collection('products');
+
+
+        app.get('/home', async (req, res) => {
+            const query = {}
+            const cursor = categoryCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('server is running')
 })
 
 app.listen(port, () => {
     console.log(`Server running on ${port}`)
+
 })
